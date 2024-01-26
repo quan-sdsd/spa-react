afc765578081eaf79b672425c465cf16c1e6b748
fromWork <quanhoang765e@gmail.com>
2024-01-26 17:47:49 +0700

    commit

--- src/component/site-top-bar.js
+++ src/component/site-top-bar.js
@@ -1,19 +1,22 @@
-import { MdEmail } from "react-icons/md";
+import { MdSearch  } from "react-icons/md";
 import { BsFillTelephoneFill } from "react-icons/bs";
 export default function SiteTopBar(props) {
-    const {phone, email} = props;
+    const {phone} = props;
     return (
         <div className="site-top-bar-container" >
             <div className="site-top-bar">
                 <img className="logo" src={require('../resource/logo.png')} alt="logo"/>
-                <div className="contract-info">
-                    <div className="contract-info-detail">
-                        <BsFillTelephoneFill className="left-icon" />
-                        <span>{phone}</span>
+                <div className="top-bar-right-box">
+                    <div className="search-box">
+                        <input className="search-area" type="text" placeholder="Tìm kiếm"/>
+                        <MdSearch className="right-icon" />
                     </div>
                     <div className="contract-info-detail">
-                        <MdEmail className="left-icon" />
-                        <span>{email}</span>
+                        <div>
+                            <BsFillTelephoneFill className="left-icon" />
+                            <span>Liên hệ để được tư vấn trực tiếp</span>
+                        </div>
+                        <span>{phone}</span>
                     </div>
                 </div>
             </div>
